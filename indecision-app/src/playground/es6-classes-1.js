df0c6bca2ee2370b6071d0f5114fd3b1a4351511
Wan Zou <wanzou1126@gmail.com>
2020-09-12 17:37:48 +1000

    Add files via upload
    added a sub class

--- indecision-app/src/playground/es6-classes-1.js
+++ indecision-app/src/playground/es6-classes-1.js
@@ -9,5 +9,22 @@ class Person {
     }
 }
 
+class Student extends Person {
+    constructor(name = "Nobody", age=0, major) {
+        super(name, age)
+        this.major = major
+    }
+
+    hasMajor() {
+        return !!this.major
+    }
+
+    getGreeting() {
+        return `hi, ${this.name} is ${this.age} year(s) old and my major is ${this.major}`;
+    }
+}
+
 const me = new Person('Vicky', 5);
 console.log(me.getGreeting());
+
+const student = new Student('Vicky', 5, "IT");
