3949d4944bbc7cd25764b6b99143f25e783d312e
Wan Zou <wanzou1126@gmail.com>
2020-09-12 20:34:53 +1000

    Add files via upload
    component props

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,10 +1,13 @@
 class Indecision extends React.Component {
     render() {
+        const title = "Indecision" 
+        const subtitle = "Put your life in the hands of a computer"
+        const options = ["Thing one", "Thing two"]
         return (
             <div>
-                <Header />
+                <Header title={title} subtitle={subtitle} />
                 <Action />
-                <Options />
+                <Options options={options} />
                 <AddOption />
             </div>
         )
@@ -13,10 +16,11 @@ class Indecision extends React.Component {
 
 class Header extends React.Component {
     render() {
+        
         return (
             <div>
-                <h1>Indecision</h1>
-                <h2>Put your life in the hands of a computer</h2>
+                <h1>{this.props.title}</h1>
+                <h2>{this.props.subtitle}</h2>
             </div>
         )
     }
@@ -34,10 +38,13 @@ class Action extends React.Component {
 
 class Options extends React.Component {
     render() {
+        console.log(this.props.options)
         return (
             <div>
                 <p>Options Component here</p>
-                <Option />
+                {
+                    this.props.options.map((option) => <Option key={option} optionText={option} />)
+                }
             </div>
         )
     }
@@ -47,7 +54,7 @@ class Option extends React.Component {
     render() {
         return (
             <div>
-                <p>Option Component here</p>
+                <p key={this.props.option}>{this.props.optionText}</p>
             </div>
         )
     }
