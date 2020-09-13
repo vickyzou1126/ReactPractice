67579b577a0fbb7206a5a76a3f43eb94cb9e1bd9
Wan Zou <wanzou1126@gmail.com>
2020-09-13 16:07:18 +1000

    Add files via upload
    re-do counter by using component and state

--- indecision-app/src/playground/counter-example.js
+++ indecision-app/src/playground/counter-example.js
@@ -13,3 +13,55 @@ const templateTwo = (
         }}>reset</button>
     </div>
 );
+
+class Counter extends React.Component {
+    constructor(props) {
+        super(props)
+        this.CounterPlus = this.CounterPlus.bind(this)
+        this.CounterMinus = this.CounterMinus.bind(this)
+        this.CounterReset = this.CounterReset.bind(this)
+        this.state = {
+            count: 0
+        }
+    }
+
+    CounterPlus() {
+        this.setState((previousState) => {
+            return {
+                count: previousState.count + 1
+            }
+        })
+    }
+
+    CounterMinus() {
+        this.setState((previousState) => {
+            if (previousState.count == 0) {
+                previousState.count = 1
+            }
+            return {
+                count: previousState.count - 1
+            }
+        })
+    }
+    
+    CounterReset() {
+        this.setState(() => {
+            return {
+                count: 0
+            }
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Count: {this.state.count}</h1>
+                <button onClick={this.CounterPlus}>+1</button>
+                <button onClick={this.CounterMinus}>-1</button>
+                <button onClick={this.CounterReset}>reset</button>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<Counter />, document.getElementById("app"));
